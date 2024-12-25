ff1e1adeec3f1292b5152de0f1376af6371b5fa6
Ralf Aron <ralf.aron@iosb-ina.fraunhofer.de>
2024-12-25 12:54:44 +0100

    fix: aas-server renamed to aas-node

---
+++ projects/aas-node/src/app/aas-node.ts
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2019-2024 Fraunhofer IOSB-INA Lemgo,
+ * eine rechtlich nicht selbstaendige Einrichtung der Fraunhofer-Gesellschaft
+ * zur Foerderung der angewandten Forschung e.V.
+ *
+ *****************************************************************************/
+
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { UserStorageFactory } from './auth/user-storage-factory.js';
+import { LoggerFactory } from './logging/logger-factory.js';
+import { FileLogger } from './logging/file-logger.js';
+import { WSNode } from './ws-node.js';
+import { AASProvider } from './aas-provider/aas-provider.js';
+import { AASIndexFactory } from './aas-index/aas-index-factory.js';
+import { TemplateStorage } from './template/template-storage.js';
+
+container.registerInstance('USERS_DIR', './users');
+container.registerSingleton('Logger', FileLogger);
+container.register('AASIndex', { useFactory: c => new AASIndexFactory(c).create() });
+container.register('UserStorage', { useFactory: c => new UserStorageFactory(c).create() });
+container.register('winston.Logger', { useFactory: () => new LoggerFactory().create() });
+
+container.afterResolution(
+    AASProvider,
+    (_, instance) => {
+        (instance as AASProvider).start(container.resolve(WSNode));
+    },
+    { frequency: 'Once' },
+);
+
+container.resolve(WSNode).run();
+container.resolve(AASProvider);
+container.resolve(TemplateStorage).start();
