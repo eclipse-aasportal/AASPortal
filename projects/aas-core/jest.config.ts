454e44f8c7e2c7acb2138b7880104e7810e1dbef
Ralf Aron <ralf.aron@iosb-ina.fraunhofer.de>
2025-06-26 17:40:51 +0200

    fix: update test system

--- projects/aas-core/jest.config.ts
+++ projects/aas-core/jest.config.ts
@@ -1,30 +1,26 @@
 import type { Config } from 'jest';
+import { createDefaultEsmPreset } from 'ts-jest';
+
+const presetConfig = createDefaultEsmPreset({
+    tsconfig: './tsconfig.spec.json',
+});
 
 const config: Config = {
+    ...presetConfig,
     clearMocks: true,
     collectCoverage: true,
     collectCoverageFrom: ['<rootDir>/src/lib/**/*.ts'],
     coverageDirectory: '../../reports/aas-core',
     coverageReporters: ['html', 'json-summary', 'cobertura'],
     coverageProvider: 'v8',
-    extensionsToTreatAsEsm: ['.ts'],
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
     },
-    preset: 'ts-jest/presets/default-esm',
     reporters: ['default', ['jest-junit', { outputDirectory: '../../reports', outputName: 'aas-core.xml' }]],
     rootDir: '.',
     roots: ['<rootDir>/src/'],
     testEnvironment: 'node',
     testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                useESM: true,
-            },
-        ],
-    },
 };
 
 export default config;
