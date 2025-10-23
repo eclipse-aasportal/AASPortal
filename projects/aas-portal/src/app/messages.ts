d1406fdba71cdfdfa506e376301cfbe7a6307e3b
Ralf Aron <ralf.aron@iosb-ina.fraunhofer.de>
2025-10-23 16:30:09 +0200

    fix: update message.ts

--- projects/aas-portal/src/app/messages.ts
+++ projects/aas-portal/src/app/messages.ts
@@ -6,20 +6,6 @@
  *
  *****************************************************************************/
 
-<<<<<<<< HEAD:projects/aas-package/src/lib/types.ts
-export type FileResult = {
-    filename: string;
-    value: string;
-    readable: NodeJS.ReadableStream;
-    size?: number;
-    contentType?: string;
-};
-
-export interface HTMLDocumentElement extends HTMLElement {
-    _nsMap: { [key: string]: string };
-}
-========
 export const INFO = {
     FILE_SUCCESSFULLY_UPLOADED: 'Info.FILE_SUCCESSFULLY_UPLOADED',
 };
->>>>>>>> refactor/download:projects/aas-portal/src/app/messages.ts
