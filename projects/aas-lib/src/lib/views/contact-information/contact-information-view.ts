b17a038c112df3f964750fcf60eb44b69fd2c9b6
Alexander Wollbrink <alexander.wollbrink@iosb-ina.fraunhofer.de>
2026-08-19 09:43:34 +0200

    redesigned contact information view

--- projects/aas-lib/src/lib/views/contact-information/contact-information-view.ts
+++ projects/aas-lib/src/lib/views/contact-information/contact-information-view.ts
@@ -19,6 +19,7 @@ import { LeafView } from '../leaf-view';
 import { StartService } from '../../shared/services/start.service';
 import { encodeBase64Url } from '../../utilities';
 import { VIEW_ROUTE_NAME } from '../view-route-name';
+import { RouterModule } from '@angular/router';
 
 /**
  * Provides a view for contact information submodels.
@@ -28,7 +29,7 @@ import { VIEW_ROUTE_NAME } from '../view-route-name';
     templateUrl: './contact-information-view.html',
     styleUrls: ['./contact-information-view.scss'],
     providers: [{ provide: VIEW_ROUTE_NAME, useValue: 'ContactInformation' }],
-    imports: [TranslateDirective, NgbPaginationModule, NgbAccordionModule, ThumbnailQRCode, ContactInformation],
+    imports: [TranslateDirective, NgbPaginationModule, NgbAccordionModule, ThumbnailQRCode, ContactInformation, RouterModule],
 })
 export class ContactInformationView extends LeafView implements OnDestroy {
     private readonly toolbar = inject(ToolbarService);
